866c4010c3f31af536e0a1d2672f549a1ec3b06d
Bret <bret.combe@gmail.com>
2023-10-04 21:55:46 -0700

    release changes

--- scripts/dashed-line.js
+++ scripts/dashed-line.js
@@ -20,11 +20,6 @@ function run(context){
 				product.name,
 				"Script Name"
 			),
-			parameters: context.jobs.productProperty(
-				context.job.id,
-				product.name,
-				"Script Parameters"
-			),
             pockets: context.jobs.productProperty(
                 context.job.id,
                 product.name,
@@ -75,6 +70,9 @@ function run(context){
 
         // Create penColor from first color of layout
         var penColor = new Color()
+            penColor.cyan = 0;
+            penColor.magenta = 0;
+            penColor.yellow = 0;
             penColor.black = 30;
         
         // Create pen with penColor and thickness
@@ -84,29 +82,99 @@ function run(context){
             painter.pen = pen;
 
         var barRect
+        var dims = {
+            width: product.rotation == 90 ? specs.height*specs.scale.height*72 : specs.width*specs.scale.width*72,
+            height: product.rotation == 90 ? specs.width*specs.scale.width*72 : specs.height*specs.scale.height*72
+        }
 
+        // Set the pocket information
         var pocket = {
-            size:{
-                top: scripts.pockets.split(',')[0].split(':')[2],
-                bottom: scripts.pockets.split(',')[1].split(':')[2],
-                left: scripts.pockets.split(',')[2].split(':')[2],
-                right: scripts.pockets.split(',')[3].split(':')[2]
-            },
-            side:{
+            side: {
                 top: scripts.pockets.split(',')[0].split(':')[1] == "true",
                 bottom: scripts.pockets.split(',')[1].split(':')[1] == "true",
                 left: scripts.pockets.split(',')[2].split(':')[1] == "true",
                 right: scripts.pockets.split(',')[3].split(':')[1] == "true"
+            },
+            size: {
+                top: scripts.pockets.split(',')[0].split(':')[2],
+                bottom: scripts.pockets.split(',')[1].split(':')[2],
+                left: scripts.pockets.split(',')[2].split(':')[2],
+                right: scripts.pockets.split(',')[3].split(':')[2]
             }
         }
-        
-        // If it has a top pocket (including top and bottom), then draw the shape and return out before drawing bottom.
-        //if(scripts.parameters == "T" || scripts.parameters == "TB"){
+
+        if(product.rotation == 90){
+            var difference = {
+                width: product.globalRect.width-(specs.height*specs.scale.height*72),
+                height: product.globalRect.height-(specs.width*specs.scale.width*72)
+            }
+
+            var anchor = {
+                top: product.position.y+difference.height,
+                left: product.position.x
+            }
+
+            var hem = {
+                top: pocket.size.top != 0 ? pocket.size.top*72 : pocket.size.bottom != 0 ? (difference.height-(pocket.size.bottom*72)) : difference.height/2,
+                left: pocket.size.left != 0 ? pocket.size.left*72 : pocket.size.right != 0 ? (difference.width-(pocket.size.right*72)) : difference.width/2
+            }
+
+            // Set the base position. This is also the default for bottom or right pockets only.
+            var position = {
+                top: anchor.top-hem.top,
+                left: anchor.left+hem.left
+            }
+
+        }else{
+            var difference = {
+                width: product.globalRect.width-(specs.width*specs.scale.width*72),
+                height: product.globalRect.height-(specs.height*specs.scale.height*72)
+            }
+
+            var anchor = {
+                top: product.position.y+difference.height,
+                left: product.position.x
+            }
+
+            var hem = {
+                top: pocket.size.top != 0 ? pocket.size.top*72 : pocket.size.bottom != 0 ? (difference.height-(pocket.size.bottom*72)) : difference.height/2,
+                left: pocket.size.left != 0 ? pocket.size.left*72 : pocket.size.right != 0 ? (difference.width-(pocket.size.right*72)) : difference.width/2
+            }
+
+            // Set the base position. This is also the default for bottom or right pockets only.
+            var position = {
+                top: anchor.top-hem.top,
+                left: anchor.left+hem.left
+            }
+        }
+
         if(pocket.side.top){
+            //position.top = product.rotation == 90 ? product.position.y+((product.globalRect.height-(specs.width*specs.scale.width*72))/2) : product.position.y+(product.globalRect.height-(specs.height*specs.scale.height*72))-(pocket.size.top*72)-(1*72)
+            //position.top = product.rotation == 90 ? product.position.x+(pocket.size.top*72)+(1*72) : product.position.y+(product.globalRect.height-(specs.height*specs.scale.height*72))-(pocket.size.top*72)-(1*72)
+        }
+
+        if(pocket.side.left){
+            //position.left = product.position.x+(pocket.size.left*72)+(1*72)
+        }
+
+        barRect = new Rect(
+            position.left,
+            position.top,
+            dims.width,
+            dims.height
+        );
+
+
+
+
+
+        /*
+        // If it has a top pocket (including top and bottom), then draw the shape and return out before drawing bottom.
+        if(scripts.parameters == "T" || scripts.parameters == "TB"){
             // Draw the shape
             if(product.rotation == 90){
                 barRect = new Rect(
-                    product.position.x-product.globalRect.width+(pocket.size.top*72)+(1*72),
+                    product.position.x-product.globalRect.width+(pocket.top*72)+(1*72),
                     product.position.y+((product.globalRect.height-(specs.width*specs.scale.width*72))/2),
                     (specs.height*specs.scale.height*72),
                     ((specs.width*specs.scale.width*72))
@@ -114,7 +182,7 @@ function run(context){
             }else{
                 barRect = new Rect(
                     product.position.x+((product.globalRect.width-(specs.width*specs.scale.width*72))/2),
-                    product.position.y+(product.globalRect.height-(specs.height*specs.scale.height*72))-(pocket.size.top*72)-(1*72),
+                    product.position.y+(product.globalRect.height-(specs.height*specs.scale.height*72))-(pocket.top*72)-(1*72),
                     (specs.width*specs.scale.width*72),
                     (specs.height*specs.scale.height*72)
                 );
@@ -122,12 +190,11 @@ function run(context){
         }
 
         // If it's only a bottom pocket, then draw the shape.
-        //if(scripts.parameters == "B"){
-        if(pocket.side.bottom && !pocket.side.top){
+        if(scripts.parameters == "B"){
             // Draw the shape
             if(product.rotation == 90){
                 barRect = new Rect(
-                    product.position.x-(specs.height*72)-(pocket.size.bottom*72)-(1*72),
+                    product.position.x-(specs.height*72)-(pocket.bottom*72)-(1*72),
                     product.position.y+((product.globalRect.height-(specs.width*specs.scale.width*72))/2),
                     (specs.height*specs.scale.height*72),
                     ((specs.width*specs.scale.width*72))
@@ -135,12 +202,13 @@ function run(context){
             }else{
                 barRect = new Rect(
                     product.position.x+((product.globalRect.width-(specs.width*specs.scale.width*72))/2),
-                    product.position.y+(pocket.size.bottom*72)+(1*72),
+                    product.position.y+(pocket.bottom*72)+(1*72),
                     (specs.width*specs.scale.width*72),
                     (specs.height*specs.scale.height*72)
                 );
             }
         }
+        */
 
         // Draw the dashed line based on the above settings.
         painter.draw(barRect);
